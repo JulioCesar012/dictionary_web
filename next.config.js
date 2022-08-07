037f9a8d81e430a896e0d9ded547f5c090302bad
Julio Cesar Santos <rodriguesjuliocesar145@gmail.com>
2022-08-07 12:55:57 -0300

    fix: Ajusta detalhes de service worker da aplicaçao

--- next.config.js
+++ next.config.js
@@ -1,31 +1,19 @@
-const withImages = require("next-images");
 const withPwa = require("next-pwa");
 
 /** @type {import('next').NextConfig} */
-module.exports = withImages([
-  {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    webpack5: true,
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false };
-
-      return config;
-    },
-    env: {
-      API_JSON_SERVER: process.env.API_JSON_SERVER,
-    },
+module.exports = withPwa({
+  typescript: {
+    ignoreBuildErrors: true,
   },
-  [
-    withPwa,
-    {
-      pwa: {
-        disable: process.abort.NODE_ENV !== "production",
-        dest: "public",
-        register: true,
-        sw: "/sw.ts",
-      },
-    },
-  ],
-]);
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+  env: {
+    API_JSON_SERVER: process.env.API_JSON_SERVER,
+  },
+  pwa: { dest: "public",
+  },
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,24 @@ import Layout from "../components/Layout";
 import GlobalStyle from "../styles/Global";
 import "~/styles/_fonts.css";
 import { ContextProvider } from "~/utils";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
   return (
     <ContextProvider>
       <Layout>
